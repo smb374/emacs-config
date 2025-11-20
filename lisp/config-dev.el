01fcf139d319419821fd5c13e995fac5296eb57f
Po-Yeh Chen <snlk374@gmail.com>
2025-11-20 18:56:21 -0500

    Fixed fringe display in GUI mode.

--- lisp/config-dev.el
+++ lisp/config-dev.el
@@ -18,7 +18,9 @@
 
 (use-package git-gutter-fringe
   :straight t
+  :after git-gutter
   :config
+  (fringe-mode '(8 . 8))
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
